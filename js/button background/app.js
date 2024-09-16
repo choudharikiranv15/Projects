23d899d2bfa52933e502f584c044de870fc94670
KIRAN CHOUDHARI <135215593+choudharikiranv15@users.noreply.github.com>
2024-09-16 16:55:42 +0530

    Add files via upload

---
+++ js/button background/app.js
@@ -0,0 +1,13 @@
+const makeRandColor = () => {
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    return `rgb(${r},${g},${b})`
+}
+const buttons = document.querySelectorAll('button');
+for (let button of buttons){
+    button.addEventListener('click',function(){
+        button.style.backgroundColor = makeRandColor();
+        button.style.color= makeRandColor();
+    })
+}
